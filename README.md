b8f1b5a6006294b59c091116a28f44499d071ad1
Paulo Torrealba <paulo@buscalibre.com>
2022-11-14 03:39:16 -0300

    Click en sombra del menu y esconder carrito

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # VERDURIZATE !
 
 - Descripcion para la canasta de productos
-- Click en sombra del menu y esconder carrito
 - Agregar al localStorage el precio
 - "Mi Cuenta"?
--- js/index.js
+++ js/index.js
@@ -8,9 +8,18 @@ $("nav.categorias ul.categorias li").on("click", function () {
 	$("nav.categorias ul.categorias li").removeClass("activo");
 	$(this).addClass("activo");
 });
+$("nav.categorias > ul > li").on("click", function () {
+	const categoria = $(this).html();
+	if ( categoria === "todos los productos" ) {
+		$(".productos .producto").show();
+	} else {
+		$(".productos .producto").hide();
+		$(`.productos .producto[data-categoria="${categoria}"]`).show();
+	}
+});
 
 // X del carro de compras
-$("section.productosCarrito_contenido header img.cerrar").on("click", () => {
+$("section.productosCarrito_contenido header img.cerrar, section.productosCarrito_fondo").on("click", () => {
 	$(".productosCarrito_fondo").hide();
 	$(".productosCarrito_contenido").removeClass("desplegar");
 });
@@ -26,21 +35,10 @@ $("header.principal .menu ul li.carro").on("click", () => {
 });
 
 // MOBILE
-// Categorias
-$("nav.categorias > ul > li").on("click", function () {
-	const categoria = $(this).html();
-	if ( categoria === "todos los productos" ) {
-		$(".productos .producto").show();
-	} else {
-		$(".productos .producto").hide();
-		$(`.productos .producto[data-categoria="${categoria}"]`).show();
-	}
-});
-
-// MOBILE
+// NAV CATEGORIAS
 $("nav.categorias p.mobile").on("click", () => {
 	$("nav.categorias ul.categorias").fadeToggle();
-	$("nav.categorias p.mobile").toggle()
+	$("nav.categorias p.mobile").toggle();
 });
 
 trae();
